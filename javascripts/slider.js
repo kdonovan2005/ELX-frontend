204b94c68dca8c7be9cf4d717d72ca6df476c287
kdonovan2005 <kdonovan2005@gmail.com>
2016-10-22 11:42:52 -0400

    Fixed JS slider rotation

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -13,7 +13,9 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n >= slides.length) { slideIndex = 0 }
+  // cycles back to beginning if at the end of the slides when next button
+  if (n > slides.length) { slideIndex = 1 }
+  // cycles back to end if at the beginning of the slides when prev button
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
@@ -21,6 +23,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1 ].className += " active";
 }
