11ba32636c21e8aa1c935a2594b29f01428a557f
kdonovan2005 <kdonovan2005@gmail.com>
2016-10-26 02:22:18 -0400

    fixed javascript conflicts on product detail page

--- javascripts/product-tabs.js
+++ javascripts/product-tabs.js
@@ -25,4 +25,19 @@ window.onload = function() {
   defaultOpen.click();
   defaultOpenFeature = document.getElementById("defaultOpenFeature");
   defaultOpenFeature.click();
+
+  var modal = document.getElementsByClassName("my-list-modal")[0];
+
+  var btn = document.getElementById("my-list-image");
+
+  var close = document.getElementsByClassName("close-modal-button")[0]
+
+  btn.onclick = function() {
+    debugger
+    modal.style.display = "block";
+  }
+
+  close.onclick = function() {
+    modal.style.display = "none";
+  }
 }
